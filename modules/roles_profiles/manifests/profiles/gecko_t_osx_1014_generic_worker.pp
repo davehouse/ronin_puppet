b63aec22e2c416bd09363afe48af965c87d085f5
davehouse <dhouse@mozilla.com>
2019-10-11 15:53:17 -0600

    inv chars

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
@@ -22,9 +22,9 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker {
             class { 'puppet::atboot':
                 telegraf_user     => lookup('telegraf.user'),
                 telegraf_password => lookup('telegraf.password'),
-                puppet_repo       => 'https://github.com/davehouse/ronin_puppet.git',¬
-                puppet_branch     => 'bug1585750_mac-stats_testing',¬
-                puppet_notify_email => 'dhouse@mozilla.com',¬
+                puppet_repo       => 'https://github.com/davehouse/ronin_puppet.git',
+                puppet_branch     => 'bug1585750_mac-stats_testing',
+                puppet_notify_email => 'dhouse@mozilla.com',
                 # Note the camelCase key names
                 meta_data         => $meta_data,
             }
