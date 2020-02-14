d8b9e5226d5b05b93394897f16f39062a888ce0f
davehouse <dhouse@mozilla.com>
2020-02-14 16:47:07 -0700

    pin to testing branch

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
@@ -22,6 +22,11 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker {
             class { 'puppet::atboot':
                 telegraf_user     => lookup('telegraf.user'),
                 telegraf_password => lookup('telegraf.password'),
+                puppet_env        => 'dev',
+                puppet_repo       => 'https://github.com/davehouse/ronin_puppet.git',
+                puppet_branch     => 'bug1615585_chrome-v80-testing',
+                puppet_notify_email => 'dhouse@mozilla.com',
+
                 # Note the camelCase key names
                 meta_data         => $meta_data,
             }
