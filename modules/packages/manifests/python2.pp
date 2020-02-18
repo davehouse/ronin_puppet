c4980199abd8fe2103c4cc6553eeff7b5b8ddf56
davehouse <dhouse@mozilla.com>
2020-02-18 10:15:03 -0700

    pin macos python2 to 3a877e3

--- modules/packages/manifests/python2.pp
+++ modules/packages/manifests/python2.pp
@@ -4,7 +4,7 @@
 
 class packages::python2 {
 
-    package { 'python2':
+    package { 'https://raw.githubusercontent.com/Homebrew/homebrew-core/3a877e3525d93cfeb076fc57579bdd589defc585/Formula/python@2.rb':
         ensure   => present,
         provider => brew,
     }
