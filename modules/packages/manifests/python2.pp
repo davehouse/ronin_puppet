526fb6a14ee4841cf1451050a02fa3c7bd6ca9d2
davehouse <dhouse@mozilla.com>
2020-02-19 09:26:01 -0700

    add tap

--- modules/packages/manifests/python2.pp
+++ modules/packages/manifests/python2.pp
@@ -4,6 +4,10 @@
 
 class packages::python2 {
 
+    package { 'davehouse/homebrew-moz':
+        ensure   => present,
+        provider => tap,
+    }
     # brew install davehouse/homebrew-moz/python@2
     package { 'davehouse/homebrew-moz/python@2': #https://raw.githubusercontent.com/Homebrew/homebrew-core/3a877e3525d93cfeb076fc57579bdd589defc585/Formula/python@2.rb':
         ensure   => present,
