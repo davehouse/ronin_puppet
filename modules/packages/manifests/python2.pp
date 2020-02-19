5ff01a2f04d5e6effe1201a1d60c6cdfc4327e2d
davehouse <dhouse@mozilla.com>
2020-02-19 09:05:35 -0700

    present not latest

--- modules/packages/manifests/python2.pp
+++ modules/packages/manifests/python2.pp
@@ -6,7 +6,7 @@ class packages::python2 {
 
     # brew install davehouse/homebrew-moz/python@2
     package { 'davehouse/homebrew-moz/python@2': #https://raw.githubusercontent.com/Homebrew/homebrew-core/3a877e3525d93cfeb076fc57579bdd589defc585/Formula/python@2.rb':
-        ensure   => latest,
+        ensure   => present,
         provider => brew,
         install_options => [ '--link' ],
     }
