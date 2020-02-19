9de5f0ddd6984dc5110da702ed7b2503cfbaeebb
davehouse <dhouse@mozilla.com>
2020-02-18 18:11:43 -0700

    fix python2 pip dependency

--- modules/packages/manifests/python2.pp
+++ modules/packages/manifests/python2.pp
@@ -4,15 +4,14 @@
 
 class packages::python2 {
 
-    package { "python2":
-        source   => 'https://raw.githubusercontent.com/Homebrew/homebrew-core/3a877e3525d93cfeb076fc57579bdd589defc585/Formula/python@2.rb',
+    package { 'https://raw.githubusercontent.com/Homebrew/homebrew-core/3a877e3525d93cfeb076fc57579bdd589defc585/Formula/python@2.rb':
         ensure   => present,
         provider => brew,
+        notify   => Exec['pip2'],
     }
 
     exec { "pip2":
-        command     => "pip install --upgrade pip setuptools",
+        command     => "/usr/local/bin/pip install --upgrade pip setuptools",
         refreshonly => true,
-        subscribe   => Package["python2"],
     }
 }
