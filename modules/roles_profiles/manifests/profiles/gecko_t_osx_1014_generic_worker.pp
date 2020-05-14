7d5655a8c5324bb3d0b2b14b7a8d852048914bc7
davehouse <dhouse@mozilla.com>
2020-05-14 10:11:23 -0600

    pin to dev branch

--- modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_osx_1014_generic_worker.pp
@@ -24,7 +24,7 @@ class roles_profiles::profiles::gecko_t_osx_1014_generic_worker {
                 telegraf_password => lookup('telegraf.password'),
                 puppet_env          => 'dev',
                 puppet_repo         => 'https://github.com/davehouse/ronin_puppet.git',
-                puppet_branch       => 'bug1622827_macos-chrome-v81-test',
+                puppet_branch       => 'bug1634202_macos-chrome-autoupdate',
                 puppet_notify_email => 'dhouse@mozilla.com',
                 # Note the camelCase key names
                 meta_data         => $meta_data,
