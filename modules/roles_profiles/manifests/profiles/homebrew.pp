b43767005cbb096037173315ac946189210383b0
davehouse <dhouse@mozilla.com>
2020-05-15 11:17:38 -0600

    set homebrew no update env var

--- modules/roles_profiles/manifests/profiles/homebrew.pp
+++ modules/roles_profiles/manifests/profiles/homebrew.pp
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class roles_profiles::profiles::homebrew {
+class roles_profiles::profiles::homebrew (
+    Boolean $update = false,
+) {
 
     require packages::xcode_cmd_line_tools
     require packages::coreutils
@@ -14,4 +16,13 @@ class roles_profiles::profiles::homebrew {
         multiuser => true,
         require   => Class['packages::xcode_cmd_line_tools'],
     }
+
+    $update_value = $update ? { false => 1, default => 0 }
+    file { '/etc/environment': ensure => present }
+    -> file_line { 'homebrew-disable-updates':
+        path  => '/etc/environment',
+            line  => "HOMEBREW_NO_AUTO_UPDATE=${update_value}",
+            match => '^HOMEBREW_NO_AUTO_UPDATE',
+    }
+
 }
