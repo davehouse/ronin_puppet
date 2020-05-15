5b0fd579095211b04738afed684ce60dd57c0506
davehouse <dhouse@mozilla.com>
2020-05-15 11:50:27 -0600

    pin homebrew-core repo

--- modules/roles_profiles/manifests/profiles/homebrew.pp
+++ modules/roles_profiles/manifests/profiles/homebrew.pp
@@ -10,18 +10,25 @@ class roles_profiles::profiles::homebrew (
     require packages::coreutils
     require roles_profiles::profiles::cltbld_user
 
+    $formula_hash = '3a877e3525d93cfeb076fc57579bdd589defc585'
+
     class { 'homebrew':
         user      => 'cltbld',
         group     => 'staff',
         multiuser => true,
         require   => Class['packages::xcode_cmd_line_tools'],
     }
+    -> file { '/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/.git/refs/heads/master':
+        ensure  => file,
+        content => "${formula_hash}\n",
+        owner   => 'root',
+        mode    => '0644',
+    }
 
-    $update_value = $update ? { false => 1, default => 0 }
     file { '/etc/environment': ensure => present }
     -> file_line { 'homebrew-disable-updates':
         path  => '/etc/environment',
-            line  => "HOMEBREW_NO_AUTO_UPDATE=${update_value}",
+            line  => $update ? { false => 'HOMEBREW_NO_AUTO_UPDATE=1', default => '' },
             match => '^HOMEBREW_NO_AUTO_UPDATE',
     }
 
