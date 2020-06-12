cc662a5308690bebd6bbc36671ed0cc93c9149c6
davehouse <dhouse@mozilla.com>
2020-06-12 08:00:57 -0600

    re-build mercurial as dmg

--- modules/packages/manifests/mercurial.pp
+++ modules/packages/manifests/mercurial.pp
@@ -3,15 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::mercurial (
-    Pattern[/^\d+\.\d+(\.\d+)?_?\d*$/] $version = '5.1',
+    Pattern[/^\d+\.\d+(\.\d+)?_?\d*$/] $version = '5.1.1_2',
 ) {
-
-    # https://www.mercurial-scm.org/mac/binaries/Mercurial-5.1-macosx10.14.pkg
-    # bd23d361130fa4a70a7783ceb089882935444c6cc0679b50d1aa6e1bb4c4fe98
-
-    packages::macos_package_from_s3 { "Mercurial-${version}-macosx10.14.pkg":
+    packages::macos_package_from_s3 { "mercurial-${version}.dmg":
         private             => false,
         os_version_specific => false,
-        type                => 'pkg',
+        type                => 'dmg',
     }
 }
