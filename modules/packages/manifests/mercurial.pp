d0158bc66ca6526001445ad45fac855f2fe2fc7a
davehouse <dhouse@mozilla.com>
2020-06-16 09:30:58 -0600

    mercurial through pip

--- modules/packages/manifests/mercurial.pp
+++ modules/packages/manifests/mercurial.pp
@@ -3,15 +3,31 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class packages::mercurial (
-    Pattern[/^\d+\.\d+(\.\d+)?_?\d*$/] $version = '5.1',
+    Pattern[/^\d+\.\d+(\.\d+)?_?\d*$/] $version = '5.4.1',
 ) {
 
-    # https://www.mercurial-scm.org/mac/binaries/Mercurial-5.1-macosx10.14.pkg
-    # bd23d361130fa4a70a7783ceb089882935444c6cc0679b50d1aa6e1bb4c4fe98
+#     # https://www.mercurial-scm.org/mac/binaries/Mercurial-5.1-macosx10.14.pkg
+#     # bd23d361130fa4a70a7783ceb089882935444c6cc0679b50d1aa6e1bb4c4fe98
+# 
+#     packages::macos_package_from_s3 { "mercurial-${version}.dmg":
+#         private             => false,
+#         os_version_specific => false,
+#         type                => 'dmg',
+#     }
 
-    packages::macos_package_from_s3 { "Mercurial-${version}-macosx10.14.pkg":
-        private             => false,
-        os_version_specific => false,
-        type                => 'pkg',
+    require packages::python3
+    package { 'python3-mercurial':
+        ensure   => '5.4.1',
+        name     => 'mercurial',
+        provider => pip3,
+        require  => Class['packages::python3'],
+    }
+
+    require packages::python2
+    package { 'python2-mercurial':
+        ensure   => '5.4.1',
+        name     => 'mercurial',
+        provider => pip,
+        require  => Class['packages::python2'],
     }
 }
