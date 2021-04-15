7ac10ad00dd8c0b637dd7aff62fd14c78480a205
davehouse <dhouse@mozilla.com>
2021-04-14 20:05:21 -0600

    fix group for useast

--- modules/roles_profiles/manifests/profiles/gecko_t_linux_talos_generic_worker.pp
+++ modules/roles_profiles/manifests/profiles/gecko_t_linux_talos_generic_worker.pp
@@ -7,7 +7,13 @@ class roles_profiles::profiles::gecko_t_linux_talos_generic_worker {
     # TODO: make these args to this module and use in call in gecko_t_linux_talos?
     $instance_type = regsubst($facts['ec2_metadata']['instance-type'], '\.', '-', 'G')
     $worker_type  = "gecko-t-linux-talos-${instance_type}"
-    $worker_group = regsubst($facts['networking']['fqdn'], '^(.*\.releng|ip-.*)\.([^\.]+)\.(mozilla|compute)\..*', '\2')
+    $cloud_group = regsubst($facts['networking']['fqdn'], '^(.*\.releng|ip-.*)\.([^\.]+)\.(mozilla|compute)\..*', '\2')
+    if $cloud_group =~ /^ip-.*ec2\.internal$/ {
+        $worker_group = 'us-east'
+    }
+    else {
+        $worker_group = $cloud_group
+    }
     $worker_id = $facts['ec2_metadata']['instance-id']
 
     case $::operatingsystem {
